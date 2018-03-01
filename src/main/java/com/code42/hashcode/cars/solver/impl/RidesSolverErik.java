8ef486ba503222b8f84cd3a9f72c362b35141a05
Erik Birgiser <erikbirgiser@protonmail.ch>
2018-03-01 21:01:48 +0200

    RidesSolverInital

---
+++ src/main/java/com/code42/hashcode/cars/solver/impl/RidesSolverErik.java
@@ -0,0 +1,23 @@
+package com.code42.hashcode.cars.solver.impl;
+
+import com.code42.hashcode.cars.io.SolutionWriter;
+import com.code42.hashcode.cars.model.Point;
+import com.code42.hashcode.cars.model.RequestModel;
+import com.code42.hashcode.cars.solver.Solver;
+
+/**
+ * Created by erik on 3/1/18
+ */
+public class RidesSolverErik implements Solver {
+
+    private class Vechicle {
+        private Point currentPosition = new Point(0,0);
+    }
+
+
+
+    @Override
+    public void solve(RequestModel requestModel, SolutionWriter writer) {
+
+    }
+}
